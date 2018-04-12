7b68d327bf2b3210c1e5d2c3864d7011d904034f
kemenes <kemenesbalazs@gmail.com>
2018-04-12 14:30:03 +0200

    Create basic test

--- test/salecontract.js
+++ test/salecontract.js
@@ -7,21 +7,13 @@ contract('SaleContract', function (accounts) {
 
         return SaleContract.deployed().then(function (instance) {
             saleContract = instance;
-            return saleContract.createContract.call(accounts[1],{from: accounts[0]});
-        }).then(function (result) {
-            var contractId = result.toString();
-            console.log(contractId);
-            //assert.equal(contractId, "0", "The e-contract was not created");
-            return saleContract.getContracts();
-        }).then(function (contracts) {
-            console.log(contracts);
-        })
-        /*
-            return saleContract.getContractParticipants.call(0);
+            return saleContract.createContract(accounts[1],{from: accounts[0]});
+        }).then(function () {
+            return saleContract.getContractParticipants(0);
         }).then(function (participants) {
-            assert.equal(participants.seller.toNumber(), accounts[1], "Seller mismatch");
-            assert.equal(participants.buyer.toNumber(), accounts[0], "Buyer mismatch");
-        });*/
+            assert.equal(participants[1], accounts[1], "Seller mismatch");
+            assert.equal(participants[0], accounts[0], "Buyer mismatch");
+        });
     });
 
 });
