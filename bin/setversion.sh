59e1f235c390be5f88e1748e52f711cb33854d16
kemenes <kemenesbalazs@gmail.com>
2018-04-18 22:29:04 +0200

    Truffle solidity version bugfix

--- bin/setversion.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Truffle includes solidity v0.4.15 in the included solc compiler, while view was introduced in 0.4.16
-# Update solditiy version
-# https://github.com/truffle-box/react-uport-box/issues/15
-
-cd /usr/local/lib/node_modules/truffle
-npm install solc@0.4.15
--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 npm install
 npm install -g ganache-cli truffle
+
+# Force truffle to use upgraded solidity compiler
+npm install solc@0.4.18npm install solc@0.4.18
