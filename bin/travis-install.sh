cc8bcf937cddb996332ac4d4eb45ebc54770186e
kemenes <kemenesbalazs@gmail.com>
2018-04-19 01:02:27 +0200

    travis fix

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -3,6 +3,9 @@ npm install
 npm install -g ganache-cli truffle
 
 # Upgrade truffles solidity version
-cd node_modules/truffle
+pwd
+cd /home/travis/.nvm/versions/node/v9.4.0/lib/node_modules/truffle
+pwd
 npm install solc@0.4.18
 cd $TRAVIS_BUILD_DIR
+pwd
