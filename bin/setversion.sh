9bda9919ed035fac54d5214bdcb031d17db6750e
kemenes <kemenesbalazs@gmail.com>
2018-04-18 15:41:51 +0200

    Truffle solidity version bugfix

---
+++ bin/setversion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Truffle includes solidity v0.4.15 in the included solc compiler, while view was introduced in 0.4.16
+# Update solditiy version
+# https://github.com/truffle-box/react-uport-box/issues/15
+
+cd /usr/local/lib/node_modules/truffle/
+yarn add solc
