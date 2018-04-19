47eb6b12e84ed755e79f069d2164fd295d15f12b
kemenes <kemenesbalazs@gmail.com>
2018-04-19 11:23:43 +0200

    Update truffle

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -2,9 +2,7 @@
 npm install
 npm install -g ganache-cli truffle@4.1.5
 
-pwd
-cd $(npm root -g)/truffle
-pwd
-npm install solc@0.4.18
-cd $TRAVIS_BUILD_DIR
-pwd
+echo 'Local truffle'
+npm list | grep truffle
+echo 'Global truffle'
+npm list -g | grep truffle
--- contracts/SaleContract.sol
+++ contracts/SaleContract.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.18;
 
-
 import "./libs/strings.sol";
 
 
