e057ff8637f655c6eece7f566aff86bf401564b9
kemenes <kemenesbalazs@gmail.com>
2018-04-19 19:38:37 +0200

    Remove set -e

--- bin/test.sh
+++ bin/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -e
-
 ganache-cli --gasLimit 4712388 2> /dev/null 1> /dev/null &
+echo 'ganace-cli started'
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
+echo 'build removed'
 truffle compile
+echo 'truffle compile done'
 truffle migrate --reset --network development
+echo 'migrate done'
 truffle test
+echo 'all tests done'
 kill -9 $(lsof -t -i:8545)
+echo 'ganache killed'
